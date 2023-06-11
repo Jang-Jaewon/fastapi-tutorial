1ead008560fd9373cbcbd290aaed67da94b16eaa
jewon119 <jewon119@gmail.com>
2023-06-11 17:04:31 +0900

    Add: pagination test

--- main.py
+++ main.py
@@ -25,3 +25,11 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
