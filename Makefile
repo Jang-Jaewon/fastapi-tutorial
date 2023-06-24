2a3af931b775069b770c8dee47e274eafc26e160
jewon119 <jewon119@gmail.com>
2023-06-24 17:04:10 +0900

    Add: docker settings & config

---
+++ Makefile
@@ -0,0 +1,14 @@
+build:
+	docker compose up -d
+
+beauty:
+	black . && isort .
+
+db-migrate:
+	docker-compose exec web alembic revision --autogenerate -m "${MSG}"
+
+db-upgrade:
+	docker-compose exec web alembic upgrade head
+
+web-log:
+	docker-compose logs -f web
---
+++ app/main.py
@@ -0,0 +1,31 @@
+from time import time
+
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+origins = [
+    settings.CLIENT_ORIGIN,
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time()
+    response = await call_next(request)
+    process_time = time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+
+@app.get("/api/healthchecker")
+def root():
+    return {"message": "Hello World"}
