246d14da3e427f5484a1d3afcaebb981bdb57a7d
jewon119 <jewon119@gmail.com>
2023-07-14 12:55:56 +0900

    Add: Token Shema Test Case

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 from pydantic import field_validator
 
@@ -14,3 +15,8 @@ class User(CustomBaseModel):
         if not re.match("^([a-z]|[A-Z]|[0-9]|-|_|@)+$", value):
             raise ValueError("Invalid username")
         return value
+
+
+class TokenData(CustomBaseModel):
+    access_token: str
+    expires_at: datetime
--- app/test/schemas/test_user_schema.py
+++ app/test/schemas/test_user_schema.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 import pytest
 
-from app.schemas.user import User
+from app.schemas.user import TokenData, User
 
 
 def test_user_schema():
@@ -11,3 +13,13 @@ def test_user_schema():
 def test_user_schema_invalid_username():
     with pytest.raises(ValueError):
         user = User(username="jaewon!@", password="pass#")
+
+
+def test_token_date():
+    expires_at = datetime.now()
+    token_data = TokenData(access_token="test token", expires_at=expires_at)
+
+    assert token_data.model_dump() == {
+        "access_token": "test token",
+        "expires_at": expires_at,
+    }
