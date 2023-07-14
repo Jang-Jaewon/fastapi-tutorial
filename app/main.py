62fb8f0d0501cb99c6ce151ecf61e5aee4205104
jewon119 <jewon119@gmail.com>
2023-07-14 14:45:48 +0900

    Add: fastapi pagination test

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 
 from app.routers.category_routers import router as category_router
 from app.routers.product_routers import router as product_router
-from app.routers.user_routers import router as user_routes
+from app.routers.user_routers import router as user_router
+from app.routers.poc import router as poc_router
 
 app = FastAPI()
 
@@ -14,4 +15,5 @@ def health_check():
 
 app.include_router(category_router)
 app.include_router(product_router)
-app.include_router(user_routes)
+app.include_router(user_router)
+app.include_router(poc_router)
