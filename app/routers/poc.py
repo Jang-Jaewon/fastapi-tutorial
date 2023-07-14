3648954922bb7ead9257f9f45ead016632a2ae52
jewon119 <jewon119@gmail.com>
2023-07-14 16:26:04 +0900

    임시 커밋

--- app/routers/poc.py
+++ app/routers/poc.py
@@ -1,24 +1,21 @@
 from fastapi import APIRouter, Depends
-from fastapi_pagination import LimitOffsetPage, Page, add_pagination, paginate
+from fastapi_pagination import (LimitOffsetPage, LimitOffsetParams, Page,
+                                Params, add_pagination, paginate)
 from fastapi_pagination.ext.sqlalchemy import paginate as sqlalchemy_paginate
 from sqlalchemy.orm import Session
 
 from app.db.models import Category as CategoryModel
 from app.routers.deps import get_db_session
 from app.schemas.category import CategoryOutput
+from app.use_cases.poc import list_categories_uc
 
 router = APIRouter(prefix="/poc", tags=["POC"])
 
 
 @router.get("/list", response_model=Page[CategoryOutput])
 @router.get("/list/limit-offset", response_model=LimitOffsetPage[CategoryOutput])
-def list_categories():
-    categories = [
-        CategoryOutput(name=f"category {n}", slug=f"category-{n}", id=n)
-        for n in range(100)
-    ]
-
-    return paginate(categories)
+def list_categories(page: int = 1, size: int = 50, limit: int = 50, offset: int = 0):
+    return list_categories_uc(page=page, size=size)
 
 
 @router.get("/list/sqlalchemy", response_model=Page[CategoryOutput])
